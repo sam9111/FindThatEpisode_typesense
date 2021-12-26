78933c366dba26918c8aa19fdb421eb5a4da54bc
sam9111 <samyuktha19095@cse.ssn.edu.in>
2021-12-26 19:23:07 +0530

    add searchbox to backend search

--- app/controllers/episodes_controller.rb
+++ app/controllers/episodes_controller.rb
@@ -2,6 +2,7 @@ class EpisodesController < ApplicationController
   def frontend_search; end
 
   def backend_search
-    @episodes = Episode.search('jesse', 'summary', { per_page: 5, page: params[:page] })
+    @query=params[:query]
+    @episodes = Episode.search(@query, 'summary', { per_page: 5, page: params[:page] })
   end
 end
